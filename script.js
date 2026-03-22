70923cbff767872b659b96ffe57603eaed2f2970
Marvin <hohmannmarvin910@gmail.com>
2026-03-22 02:46:54 +0100

    Initial commit HTML site

---
+++ script.js
@@ -0,0 +1,9 @@
+// Beispiel: Klick auf Navigation scrollt sanft
+document.querySelectorAll('header nav a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
